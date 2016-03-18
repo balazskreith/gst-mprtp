927fd2bf36cd476ff159da44801cd9af191e9ed8
Balázs Kreith <balazs.kreith@gmail.com>
2016-03-18 10:09:17 +0100

    add mprtpdefs

---
+++ plugins/mprtpdefs.h
@@ -0,0 +1,19 @@
+/*
+ * mprtpdefs.h
+ *
+ *  Created on: Mar 14, 2016
+ *      Author: balazs
+ */
+
+#ifndef PLUGINS_MPRTPDEFS_H_
+#define PLUGINS_MPRTPDEFS_H_
+
+
+#define MPRTP_DEFAULT_EXTENSION_HEADER_ID 3
+#define ABS_TIME_DEFAULT_EXTENSION_HEADER_ID 8
+#define MONITOR_PAYLOAD_DEFAULT_ID 126
+#define SUBFLOW_DEFAULT_SENDING_RATE 150000
+
+#define DISABLE_LINE if(0)
+
+#endif /* PLUGINS_MPRTPDEFS_H_ */

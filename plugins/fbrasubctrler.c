024294c57c045e3101d03104ce9f6f90b6723d98
Balazs Kreith <balazs.kreith@gmail.com>
2016-08-12 12:02:41 +0200

    distortion remained on more conservative way

--- plugins/fbrasubctrler.c
+++ plugins/fbrasubctrler.c
@@ -530,15 +530,15 @@ done:
 
 static gboolean _distortion(FBRASubController *this)
 {
-//  GstClockTime owd_th;
-//  owd_th = _fbstat(this).owd_ltt80 + CONSTRAIN(30 * GST_MSECOND, 150 * GST_MSECOND, _fbstat(this).owd_th_cng);
-//  if(owd_th < _fbstat(this).owd_stt){
-//    return TRUE;
-//  }
-
-  if(.2 < _fbstat(this).overused_avg){
+  GstClockTime owd_th;
+  owd_th = _fbstat(this).owd_ltt80 + CONSTRAIN(30 * GST_MSECOND, 150 * GST_MSECOND, _fbstat(this).owd_th_cng);
+  if(owd_th < _fbstat(this).owd_stt){
     return TRUE;
   }
+
+//  if(.2 < _fbstat(this).overused_avg){
+//    return TRUE;
+//  }
 
   return FALSE;
 }
